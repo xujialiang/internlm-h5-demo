afff57f0a7006289be24d6dd035fc0eb9363d5ff
ElliottXU <mail@xujialiang.net>
2024-03-09 12:20:23 +0800

    init

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+# from flask import Flask, jsonify, request
 import gradio as gr
 
-def predit(name):
+# app  = Flask(__name__)
+
+def api_say_hello():
+    data = request.get_json()
+    name = data['name']
+    result = say_hello(name)
+    return jsonify(result=result)
+
+def say_hello(name):
     return "Hello" + name + "!!"
-iface = gr.Interface(fn=predit, inputs="text", outputs="text")
-iface.launch(share=True)
+iface = gr.Interface(fn=say_hello, inputs="text", outputs="text")
+iface.launch(host='0.0.0.0', port=7861)
+
+# app.run(debug=True, host='0.0.0.0', port=7861)
